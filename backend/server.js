23686346c29a86307a1e207db455889219abdf61
Jenil Sam <jenilsam2004@gmail.com>
2026-06-01 18:44:01 -0500

    added the get and post for items

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,68 @@
 import express from 'express'
-import { supabase } from './supabase';
+import { supabase } from './supabase.js';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send()
+app.use(express.json())
+
+// items
+app.post('/items', async (req, res) => {
+    const { data, error } = await supabase
+        .from('items_duplicate')
+        .insert(req.body);
+
+    res.json({ data, error })
+
 });
 
+app.get('/items', async (req, res) => {
+    const { data, error } = await supabase
+        .from('items_duplicate')
+        .select()
+
+    res.json({ data, error })
+
+});
+
+//bids
+app.post('/items', async (req, res) => {
+    const { data, error } = await supabase
+        .from('items_duplicate')
+        .insert(req.body);
+
+    res.json({ data, error })
+
+});
+
+
+//users
+app.get('/items', async (req, res) => {
+    const { data, error } = await supabase
+        .from('items_duplicate')
+        .select()
+
+    res.json({ data, error })
+
+});
+
+app.post('/items', async (req, res) => {
+    const { data, error } = await supabase
+        .from('items_duplicate')
+        .insert(req.body);
+
+    res.json({ data, error })
+
+});
+
+app.get('/items', async (req, res) => {
+    const { data, error } = await supabase
+        .from('items_duplicate')
+        .select()
+
+    res.json({ data, error })
+
+});
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
